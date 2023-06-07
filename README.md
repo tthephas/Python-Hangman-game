ffde548e40a73770305abc5b51db5cab02977969
Josh Thephasdin <47870092+tthephas@users.noreply.github.com>
2023-06-06 20:19:50 -0500

    read me added, loop for first guess built

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Python-Hangman-game
+
+
+Hangman game written in python.
+
+Asks user for a letter. 
+
+Uses a list of words to choose a random one
+
--- hangman.py
+++ hangman.py
@@ -1,18 +1,34 @@
 # build a hangman game with python
+import random
 
 word_list = ['ardvark', 'baboon', 'camel']
 
-import random
 
 chosen_word = random.choice(word_list)
 
+# 
+print(f'Psst, the word is {chosen_word}')
+
+# create empty list. for each letter in the chosen word, add "_". So if the word is apple, it should show 5 "_" representing each letter to guess.
+
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+print(display)
+
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+
+# loop thru each position in the chosen word. if the letter in that position matches the guess, then reveal that letter in the display at that position. 
+# example, if user guessed "p" and the chosen word was apple, then display should be ["_", "p", "p", "_", "_"]
+
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print('Right')
-    else:
-        print('Wrong')
+        display[position] = letter
+
+print(display)
 
 
 

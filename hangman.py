a90b1b8f114412500851025df8c446c6f7a1e7f6
Josh Thephasdin <47870092+tthephas@users.noreply.github.com>
2023-06-06 20:08:25 -0500

    start of hangman game, input built

---
+++ hangman.py
@@ -0,0 +1,19 @@
+# build a hangman game with python
+
+word_list = ['ardvark', 'baboon', 'camel']
+
+import random
+
+chosen_word = random.choice(word_list)
+
+guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print('Right')
+    else:
+        print('Wrong')
+
+
+
+

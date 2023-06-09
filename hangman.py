67c9538cd0cba3ac63fb6fa691601ad6b0e02dba
Josh Thephasdin <47870092+tthephas@users.noreply.github.com>
2023-06-08 19:48:09 -0500

    added win feature with a while loop

--- hangman.py
+++ hangman.py
@@ -1,35 +1,40 @@
 # build a hangman game with python
 import random
 
-word_list = ['ardvark', 'baboon', 'camel']
-
-
+word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-# 
-print(f'Psst, the word is {chosen_word}')
+# testing code
+print(f"Psst, the solution is {chosen_word}")
 
 # create empty list. for each letter in the chosen word, add "_". So if the word is apple, it should show 5 "_" representing each letter to guess.
 
 display = []
-word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
 print(display)
 
-guess = input("Guess a letter: ").lower()
+# using a while loop to let the user guess again. the loop only stops once the user has guessed all the letters in the chosen word, and display has no more blanks. then we'll give a winning message.
 
+end_of_game = False
 
-# loop thru each position in the chosen word. if the letter in that position matches the guess, then reveal that letter in the display at that position. 
-# example, if user guessed "p" and the chosen word was apple, then display should be ["_", "p", "p", "_", "_"]
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+    # loop thru each position in the chosen word. if the letter in that position matches the guess, then reveal that letter in the display at that position.
+    # example, if user guessed "p" and the chosen word was apple, then display should be ["_", "p", "p", "_", "_"]
 
-print(display)
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(
+            f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}"
+        )
+        if letter == guess:
+            display[position] = letter
 
+    print(display)
 
-
-
+    if "_" not in display:
+        end_of_game == True
+        print("You win.")
